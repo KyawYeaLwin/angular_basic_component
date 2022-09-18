3d4ae5cf3e7919119e2336cc99368b786b92d8b5
Kyaw Yea Lwin <kyawyealwin1995@gmail.com>
2022-09-18 13:31:05 +0630

    add third component with custom."

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SecondComponet } from './secondcomponent/second.component';
+import { ThirdComponent } from './thirdcomponent/third.component';
 @NgModule({
   declarations: [
     AppComponent,
-    SecondComponet
+    SecondComponet,
+    ThirdComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/thirdcomponent/third.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+@Component({
+    selector : 'third-component',
+    templateUrl  : './third.component.html',
+    styleUrls : ['./third.component.css']
+})
+export class ThirdComponent{}
