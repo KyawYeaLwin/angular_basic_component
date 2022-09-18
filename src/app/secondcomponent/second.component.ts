08dd2bc1a81c8f17e71d766a7199c2a012ed2f5e
Kyaw Yea Lwin <kyawyealwin1995@gmail.com>
2022-09-18 13:09:51 +0630

    First Git commit

---
+++ src/app/secondcomponent/second.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : 'second-component',
+    template: '<div>This is second component. </div>'
+
+})
+export class SecondComponet{}
